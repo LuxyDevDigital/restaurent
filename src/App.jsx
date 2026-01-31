ba4e251b4279e299e0d6ab2b37dc03e167e66b57
ibtissam <ibassma039@gmail.com>
2026-01-31 19:16:25 +0100

    Configure for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import RestaurantMenu from "./pages/RestaurantMenu"
-import Reservation from "./pages/reservation"
-import RestaurantGallerie from "./pages/gallerie"
-import AboutUs from "./pages/aboutus"
-import Footer from './components/Footer'
-import Cart from './pages/Cart'
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RestaurantMenu from "./pages/RestaurantMenu";
+import Reservation from "./pages/reservation";
+import RestaurantGallerie from "./pages/gallerie";
+import AboutUs from "./pages/aboutus";
+import Footer from "./components/Footer";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
-        <Route path="/restaurent" element={<Home/>} />
-        <Route path="/restaurent/menu" element={<RestaurantMenu/>} />
-        <Route path="/restaurent/gallerie" element={<RestaurantGallerie/>} />
-        <Route path="/restaurent/reservation" element={<Reservation/>} />
-        <Route path="/restaurent/aboutus" element={<AboutUs/>} />
-        <Route path="/restaurent/footer" element={<Footer/>} />
-        <Route path="/restaurent/cart" element={<Cart/>} />
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<RestaurantMenu />} />
+        <Route path="/gallerie" element={<RestaurantGallerie />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
